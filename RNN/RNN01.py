f2dc488eb6e0c38ffb4bc111f11e325adb079e8d
abiggg <abiggg@126.com>
2019-05-04 21:37:28 +0800

    refresh rnn

--- RNN/RNN01.py
+++ RNN/RNN01.py
@@ -1,53 +1,58 @@
-from tensorflow_train import train02_RNN as tf
-from tensorflow_train.train02_RNN import input_data
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-mnist = input_data.read_data_sets('MNIST_data',one_hot=True)
-HIDDEN_LAYER = 128
-BATCH_SIZE = 128
-INPUTS_NUM = 28
-STEPS_NUM = 28
-CLASS_NUM = 10
-learnRate = 0.001
-traning_iter = 100000
+import tensorflow as tf
+import numpy as np
 
-x = tf.placeholder(tf.float32,shape=[None,INPUTS_NUM,STEPS_NUM])
-y = tf.placeholder(tf.float32,shape=[None,CLASS_NUM])
+from tensorflow.examples.tutorials.mnist import input_data
 
-weights = {
-    'in':tf.Variable(tf.random_normal([INPUTS_NUM,HIDDEN_LAYER])),
-    'out':tf.Variable(tf.random_normal(([HIDDEN_LAYER,CLASS_NUM])))
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+INPUT_NUM = 28
+STEP_NUM = 28
+CLASSIFY_NUM = 10
+HIDDEN_LAYER_NUM = 128
+BATCHSIZE_NUM = 100
+LEARN_RATE = 0.001
+
+x = tf.placeholder(tf.float32,[None, INPUT_NUM, STEP_NUM])
+y = tf.placeholder(tf.float32, [None, CLASSIFY_NUM])
+
+weight = {
+    'in':tf.Variable(tf.random_normal([INPUT_NUM, HIDDEN_LAYER_NUM])),
+    'out': tf.Variable(tf.random_normal([HIDDEN_LAYER_NUM, CLASSIFY_NUM]))
 }
-biases = {
-    'in':tf.Variable(tf.random_normal([HIDDEN_LAYER,])),
-    'out':tf.Variable(tf.random_normal(([CLASS_NUM,])))
+
+bias = {
+    'in': tf.Variable(tf.random_normal([HIDDEN_LAYER_NUM,])),
+    'out': tf.Variable(tf.random_normal([CLASSIFY_NUM,]))
 }
 
-#key of LSTM of RNN
-def Rnn(X,weights,biases):
-    X = tf.reshape(X,[BATCH_SIZE*STEPS_NUM,INPUTS_NUM])
-    X_in = tf.matmul(X,weights['in'])+biases['in']
-    X_in = tf.reshape(X_in,[-1,STEPS_NUM,HIDDEN_LAYER])
-    cell = tf.nn.rnn_cell.BasicLSTMCell(HIDDEN_LAYER,forget_bias=1.0,state_is_tuple=True)
-    _init_state = cell.zero_state(BATCH_SIZE,tf.float32)
-    output,states = tf.nn.dynamic_rnn(cell,X_in,initial_state=_init_state,time_major=False)
-    result = tf.matmul(states[1],weights['out'])+biases['out']
+def RNN(x_orign, weight, bias):
+    x = tf.reshape(x_orign, [BATCHSIZE_NUM * STEP_NUM, INPUT_NUM])
+    x_in = tf.matmul(x, weight['in']) + bias['in']
+    x_in = tf.reshape(x_in, [-1, STEP_NUM, HIDDEN_LAYER_NUM])
+    cell = tf.nn.rnn_cell.BasicLSTMCell(HIDDEN_LAYER_NUM)
+    _init_state = cell.zero_state(BATCHSIZE_NUM, tf.float32)
+    output, states = tf.nn.dynamic_rnn(cell, x_in, initial_state = _init_state, time_major=False)
+    result = tf.matmul(states[1], weight['out']) + bias['out']
     return result
 
-pred = Rnn(x,weights,biases)
-cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred,labels= y))
-train_op = tf.train.AdamOptimizer(learnRate).minimize(cost)
+pred = RNN(x, weight, bias)
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(y, pred))
+optimizer = tf.train.AdamOptimizer(LEARN_RATE).minimize(loss)
 
-correct = tf.equal(tf.argmax(pred,1),tf.argmax(y,1))
-accuracy = tf.reduce_mean(tf.cast(correct,dtype=tf.float32))
-init = tf.initialize_all_variables()
+correct = tf.equal(tf.arg_max(y, 1), tf.arg_max(pred, 1))
+accuracy = tf.reduce_mean(tf.cast(correct, dtype=tf.float32))
 
 with tf.Session() as sess:
-    sess.run(init)
+    sess.run(tf.global_variables_initializer())
     step = 0
-    while step*BATCH_SIZE<traning_iter:
-          batch_xs,batch_ys = mnist.train.next_batch(BATCH_SIZE)
-          batch_xs = batch_xs.reshape([BATCH_SIZE,STEPS_NUM,INPUTS_NUM])
-          sess.run(train_op,feed_dict={x:batch_xs,y:batch_ys})
-          if step % 20 == 0:
-            print(sess.run(accuracy,feed_dict={x:batch_xs,y:batch_ys}))
-          step = step+1
+    while step * BATCHSIZE_NUM < 100000:
+        batch_xs, batch_ys = mnist.train.next_batch(BATCHSIZE_NUM)
+        batch_xs = batch_xs.reshape([BATCHSIZE_NUM, INPUT_NUM, STEP_NUM])
+        sess.run(optimizer, feed_dict={x:batch_xs, y:batch_ys})
+        if step % 20 == 0:
+            print(sess.run(accuracy, feed_dict={x:batch_xs, y:batch_ys}))
+        step = step + 1
+
